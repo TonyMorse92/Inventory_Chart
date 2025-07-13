ab36677f1f0357faa7b7250dc86d26ab66af635e
TonyMorse92 <anthonymorse92@gmail.com>
2025-07-13 19:38:45 -0400

    Have a line graph with api data.

--- frontend/index.js
+++ frontend/index.js
@@ -18,7 +18,6 @@ var svg = d3.select("#my_dataviz")
   // When reading the csv, I must format variables:
   function(d){
     return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
-	alert(data);
   },
 
   // Now I can use this dataset:
@@ -51,6 +50,43 @@ var svg = d3.select("#my_dataviz")
 
 })
 */
+
+  // When reading the csv, I must format variables:
+  function parse(data){
+    return { date : d3.timeParse("%Y-%m-%d")(data.date), value : data.value }
+  }
+
+  // Now I can use this dataset:
+  function make_chart(data) {
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        //.x(function(d) {  return d.date })
+        .y(function(d) { return y(d.value) })
+        )
+	};
+
 d3.request("http://127.0.0.1:8000/data/")
   .response(function(request) { return JSON.parse(request.responseText); })
   .get(function(error, data) 
@@ -58,13 +94,53 @@ d3.request("http://127.0.0.1:8000/data/")
     if (error) {
       console.error("Error fetching data:", error);
     } else {
-     	console.log(data);
+	formatted_data = []
+	data.forEach(data_point =>
+	{
+		formatted_data.push({"date": d3.timeParse("%Y-%m-%d")(data_point.date), "value": data_point.value});	
+//        alert(d3.timeParse("%Y-%m-%d")(data_point.date))
+	})
+	console.log(formatted_data);	
+}
 	
+	make_chart(formatted_data);
+
+});
+/*		
 	data.forEach(data_point =>
 	{
 		console.log(`date:  ${data_point.date}   value:  ${data_point.value}`);	
+
+		
+  // Now I can use this dataset:
+  function(data) {
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
 	});
 	// Process your data here
     }
-  });
+  });*/
 
