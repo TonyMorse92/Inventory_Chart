3ea4917980257e1ab15bd9ecae5a4f8b6d3f2ba8
TonyMorse92 <anthonymorse92@gmail.com>
2025-07-13 16:58:20 -0400

    Getting javascript set up with example line chart from d3.

--- frontend/index.js
+++ frontend/index.js
@@ -1,69 +1,53 @@
-const grid = document.getElementsByClassName("container")[0];
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-window.onload = function () 
-{
+//Read the data
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
 
-	// Make each "cell" interactive
-	for (var i = 0; i < grid.children.length; i++) 
-	{
-		grid.children[i].onclick = function()
-		{
-			sendRequest(this, replaceValue);
-			
-		}
-	}	
-}
+  // When reading the csv, I must format variables:
+  function(d){
+    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+  },
 
+  // Now I can use this dataset:
+  function(data) {
 
-function replaceValue(current_cell, fib_val)
-{
-	current_cell.innerHTML = fib_val;
-}
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-function sendRequest(current_cell, callback)
-{
-	var baseURL = "http://127.0.0.1:8000/fib/";
-	var url = baseURL + current_cell.innerHTML.trim();
-	
-	
-	var request = new XMLHttpRequest();
-	request.open('GET', url, true);
-	request.onload = function() {
-	// Begin accessing JSON data here
-	var data = JSON.parse(this.response);
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
 
-	if (this.status >= 200 && this.status < 400) 
-	{
-		callback(current_cell, data.fib_n);
-		
-	} 
-	else 
-	{
-		const errorMessage = document.createElement('marquee');
-		errorMessage.textContent = "Request failed...";
-	}
-}
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
 
+})
 
-
-	request.send()
-
-}
-
-function process()
-{
-	// Begin accessing JSON data here
-	var data = JSON.parse(this.response);
-
-	if (this.status >= 200 && this.status < 400) 
-	{
-		alert(data.fib_n);
-		
-	} 
-	else 
-	{
-		const errorMessage = document.createElement('marquee');
-		errorMessage.textContent = `Gah, it's not working`;
-	}
-}
